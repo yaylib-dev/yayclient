ec7e6d1d9dfc9febfb94954f518e8d84096cc4ff
tshima-b98 <tshima.b98@gmail.com>
2026-01-19 17:11:26 +0900

    style: Apply code formatting

--- examples/example.ts
+++ examples/example.ts
@@ -1,17 +1,17 @@
 import { Client } from 'yay.js';
 
 const main = async () => {
-	const client = new Client();
+  const client = new Client();
 
-	await client.login({
-		email: 'yourEmail',
-		password: 'yourPassword',
-	});
+  await client.login({
+    email: 'yourEmail',
+    password: 'yourPassword',
+  });
 
-	await client.createPost({
-		text: 'Hello with yay.js!',
-		sharedUrl: 'https://github.com/ekkx/yay.js',
-	});
+  await client.createPost({
+    text: 'Hello with yay.js!',
+    sharedUrl: 'https://github.com/ekkx/yay.js',
+  });
 };
 
 main();
--- examples/webSocket.ts
+++ examples/webSocket.ts
@@ -1,33 +1,30 @@
 import { Client, GatewayIntents } from 'yay.js';
 
 const client = new Client({
-	// イベントを取得する範囲を指定
-	intents: [
-        GatewayIntents.ChatMessage,
-        GatewayIntents.GroupUpdates
-    ],
+  // イベントを取得する範囲を指定
+  intents: [GatewayIntents.ChatMessage, GatewayIntents.GroupUpdates],
 });
 
 client.on('ready', () => {
-	console.log('The bot is ready!');
+  console.log('The bot is ready!');
 });
 
 // メッセージのイベントを取得
 client.on('messageCreate', async (message) => {
-	if (message.text === 'ping') {
-		await client.sendMessage({
-			text: 'pong',
-			id: message.roomId,
-		});
-	}
+  if (message.text === 'ping') {
+    await client.sendMessage({
+      text: 'pong',
+      id: message.roomId,
+    });
+  }
 });
 
 // サークル更新のイベントを取得
 client.on('groupUpdate', async (groupId) => {
-	console.log(groupId);
+  console.log(groupId);
 });
 
 client.login({
-	email: 'yourEmail',
-	password: 'yourPassword',
+  email: 'yourEmail',
+  password: 'yourPassword',
 });
